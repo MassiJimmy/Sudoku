33727f38147fb7dc0262da402a7ae4f64df82693
MassiJimmy <92297534+MassiJimmy@users.noreply.github.com>
2024-02-27 16:18:48 +0100

    begining of code

---
+++ Sudoku.js
@@ -0,0 +1,49 @@
+
+var NumSelectes = null;
+var TileSelected = null;
+
+var error = 0;
+
+
+
+var board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+var solution = [    // This is the solution to the above board
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]
+
+window.onload = function () {
+    setGame();
+}
+
+function setGame() 
+{
+    for (let i=1; i<=9; i++) 
+    {
+        // Create a div for each number
+        //<div id="1" class="number">1</div> to <div id="9" class="number">9</div>
+        let number = document.createElement("div");
+        number.id = i;
+        number.innerText = i; 
+        number.classList.add("number");
+        document.getElementById("Number").appendChild(number);
+    }
+}
