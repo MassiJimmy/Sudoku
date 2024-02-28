4da4f58254bb851336fd1ca24472eeaf99cea631
MassiJimmy <92297534+MassiJimmy@users.noreply.github.com>
2024-02-28 17:27:35 +0100

    Full game almost finished

--- Sudoku.js
+++ Sudoku.js
@@ -1,34 +1,56 @@
 
-var NumSelectes = null;
+var NumSelected = null;
 var TileSelected = null;
 
 var error = 0;
 
+function generateBoard() {
+    let board = new Array(9).fill(0).map(() => new Array(9).fill(0));
 
-var solution = [    
-    // This is the solution to the board down below
-    [5, 3, 4, 6, 7, 8, 9, 1, 2],
-    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-    [9, 6, 1, 5, 3, 7, 2, 8, 4],
-    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]
-]
+    for (let i = 0; i < 81; i++) {
+        let row = Math.floor(i / 9);
+        let col = i % 9;
+        let block = Math.floor(row / 3) * 3 + Math.floor(col / 3);
 
-var board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
+        let numbers = new Array(9).fill(0).map((_, index) => index + 1);
+        for (let j = 0; j < col; j++) {
+            const pos = numbers.indexOf(board[row][j]);
+            if (pos !== -1) numbers.splice(pos, 1);
+        }
+
+        for (let j = 0; j < row; j++) {
+            const pos = numbers.indexOf(board[j][col]);
+            if (pos !== -1) numbers.splice(pos, 1);
+        }
+
+        for (let j = 0; j < 9; j++) {
+            const pos = numbers.indexOf(board[Math.floor(block / 3) * 3 + Math.floor(j / 3)][block % 3 * 3 + j % 3]);
+            if (pos !== -1) numbers.splice(pos, 1);
+        }
+
+        let rand = Math.floor(Math.random() * numbers.length);
+        if (numbers[rand] === undefined) return generateBoard();
+        board[row][col] = numbers[rand];
+    }
+
+    return board;
+}
+
+function generateGame(board) {
+    let game = JSON.parse(JSON.stringify(board));  // Copy the board
+    for (let i = 0; i < 81; i++) {
+        // change the difficulty of the game by changing the probability of a number being removed
+        if (Math.random() < 0.75) {
+            let row = Math.floor(i / 9);
+            let col = i % 9;
+            game[row][col] = 0;
+        }
+    }
+    return game;
+}
+
+var solution = generateBoard()
+var board = generateGame(solution);
 
 window.onload = function () {
     setGame();
@@ -43,7 +65,49 @@ function setGame()
         let number = document.createElement("div");
         number.id = i;
         number.innerText = i; 
+        number.addEventListener("click", selectNumber);
         number.classList.add("number");
         document.getElementById("Number").appendChild(number);
+    }
+
+    //board 9x9
+    for(let i=0; i<9; i++)
+    {
+        for(let j=0; j<9; j++)
+        {
+            let tile = document.createElement("div");
+            tile.id = i.toString() + "-" + j.toString();
+            tile.addEventListener("click", selectTile);
+            tile.classList.add("tile");
+            tile.innerText = board[i][j] == 0 ? "" : board[i][j];
+            document.getElementById("Board").appendChild(tile);
+        }
+    }
+}
+
+function selectNumber()
+{
+    if(NumSelected != null)
+    {
+        NumSelected.classList.remove("number-selected");
+    }
+    NumSelected = this;
+    NumSelected.classList.add("number-selected");
+}
+
+function selectTile()
+{
+    if(NumSelected)
+    {
+        if(TileSelected != null)
+        {
+            TileSelected.classList.remove("tile-selected");
+        }
+        TileSelected = this;
+        TileSelected.classList.add("tile-selected");
+
+        TileSelected.innerText = NumSelected.innerText;
+        let pos = TileSelected.id.split("-");
+        board[pos[0]][pos[1]] = parseInt(NumSelected.innerText);
     }
 }
